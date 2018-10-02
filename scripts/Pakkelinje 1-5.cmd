bc5767cb0134149357178640bbfbe5de6100db74
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2018-10-02 13:59:10 +0200

    Update scripts

--- scripts/Pakkelinje 1-5.cmd
+++ scripts/Pakkelinje 1-5.cmd
@@ -3,8 +3,8 @@ SET SQLIO="C:\Source\SqlIO2\src\SQLIO2\bin\Release\netcoreapp2.1\win10-x64\SQLIO
 CHCP 65001
 TITLE Pakkelinje 1-5
 
-START /B "SB" %SQLIO% proxy -l 8077 > SB.log
-START /B "NP" %SQLIO% proxy -l 8078 -f 18078 > NP.log
-START /B "NP-NB" %SQLIO% proxy -l 8088 > NP-NB.log
-START /B "NP-NM" %SQLIO% proxy -l 8089 > NP-NM.log
-START /B "MS" %SQLIO% proxy -l 8080 > MS.log
+START /B "BS" %SQLIO% proxy -l 8077 > Pa15Bs.log
+START /B "NP" %SQLIO% proxy -l 8078 -f 18078 > Pa15Plc.log
+START /B "NP/NB" %SQLIO% proxy -l 8088 > Pa15PlcNb.log
+START /B "NP/NM" %SQLIO% proxy -l 8089 > Pa15PlcNm.log
+START /B "MS" %SQLIO% proxy -l 8080 > Pa15Ms.log
--- scripts/Pakkelinje 6.cmd
+++ scripts/Pakkelinje 6.cmd
@@ -3,8 +3,8 @@ SET SQLIO="C:\Source\SqlIO2\src\SQLIO2\bin\Release\netcoreapp2.1\win10-x64\SQLIO
 CHCP 65001
 TITLE Pakkelinje 6
 
-START /B "S5" %SQLIO% proxy -l 8215 > S5.log
-START /B "C2" %SQLIO% proxy -l 8231 -f 18231 > C2.log
-START /B "C2-NB" %SQLIO% proxy -l 8232 > C2-NB.log
-START /B "C2-NM" %SQLIO% proxy -l 8233 > C2-NM.log
-START /B "S6" %SQLIO% proxy -l 8235 > S6.log
+START /B "S5" %SQLIO% proxy -l 8215 > Pa06Bs.log
+START /B "C2" %SQLIO% proxy -l 8231 -f 18231 > Pa06Plc.log
+START /B "C2/NB" %SQLIO% proxy -l 8232 > Pa06PlcNb.log
+START /B "C2/NM" %SQLIO% proxy -l 8233 > Pa06PlcNm.log
+START /B "S6" %SQLIO% proxy -l 8235 > Pa06Ms.log
